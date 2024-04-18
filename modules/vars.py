df7a0c020bd9ac6e9a04a7f0801f838e67135577
hizhuv <167457774+hizhuv@users.noreply.github.com>
2024-04-18 22:14:55 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,4 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy bu
 api_id = "26721810"
 api_hash = "40be0b67b15513fc317a80399be79f2d"
-bot_token = ""6604626597:AAFv7FlJm73W19CitfkU5mPWTUh2XAF03cI"
+bot_token = "6604626597:AAFv7FlJm73W19CitfkU5mPWTUh2XAF03cI"
